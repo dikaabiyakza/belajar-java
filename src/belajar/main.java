9f2bf09156a7c6a3d01d0ac3d8d6692b7dd647e0
dikaabiyakza <diik.rachmat@gmail.com>
2020-07-06 22:01:53 +0700

    belajar java

---
+++ src/belajar/main.java
@@ -0,0 +1,26 @@
+package belajar;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		short umur = 19;
+		int umur1 = 1;
+		float nama = 9.5f;
+		double nama1 = 6.6;
+		
+		System.out.println("hello word");
+		
+		System.out.println(umur);
+		
+		System.out.println(umur1);
+		
+		System.out.println(nama);
+		
+		System.out.println(nama1);
+		
+		
+	}
+		
+
+}
