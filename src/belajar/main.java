a7b5a813f22bc7ae2c7643015fe5d12938e44681
dikaabiyakza <diik.rachmat@gmail.com>
2020-07-07 22:05:52 +0700

    belajar java

--- src/belajar/main.java
+++ src/belajar/main.java
@@ -5,22 +5,86 @@ public class main {
 	public static void main(String[] args) {
 		
 		short umur = 19;
-		int umur1 = 1;
-		float nama = 9.5f;
-		double nama1 = 6.6;
-		
-		System.out.println("hello word");
+		short umur1 = 18;
+		short umur2 = 17;
+		short umur3 = 16;
+		short umur4 = 15;
 		
 		System.out.println(umur);
-		
 		System.out.println(umur1);
+		System.out.println(umur2);
+		System.out.println(umur3);
+		System.out.println(umur4);
 		
-		System.out.println(nama);
+		byte name = 1;
+		byte name1 =2;
+		byte name2 =3;
+		byte name3 =4;
+		byte name4 =5;
 		
-		System.out.println(nama1);
+		System.out.println(name);
+		System.out.println(name1);
+		System.out.println(name2);
+		System.out.println(name3);
+		System.out.println(name4);
+		
+		int angka =10;
+		int angka1 =11;
+		int angka2 =12;
+		int angka3 =13;
+		int angka4 =14;
+		
+		System.out.println(angka);
+		System.out.println(angka1);
+		System.out.println(angka2);
+		System.out.println(angka3);
+		System.out.println(angka4);
+		
+		long nilai =-55;
+		long nilai1 =-56;
+		long nilai2 =-57;
+		long nilai3 =-58;
+		long nilai4 =-59;
+		
+		System.out.println(nilai);
+		System.out.println(nilai1);
+		System.out.println(nilai2);
+		System.out.println(nilai3);
+		System.out.println(nilai4);
+		
+		float bab =3.14f;
+		float bab1 =3.18f;
+		float bab2 =30.78f;
+		float bab3 =-30.8f;
+		float bab4 =-400.90f;
+		
+		System.out.println(bab);
+		System.out.println(bab1);
+		System.out.println(bab2);
+		System.out.println(bab3);
+		System.out.println(bab4);
+		
+		double pendek = -500.99;
+		double pendek1 = 500.99;
+		double pendek2 = -10000.999;
+		double pendek3 = 10000.9999;
+		double pendek4 = -123.8889;
+		
+		System.out.println(pendek);
+		System.out.println(pendek1);
+		System.out.println(pendek2);
+		System.out.println(pendek3);
+		System.out.println(pendek4);
 		
 		
+		
+			
+	}
+	
+	
+		
+				
 	}
 		
 
-}
+
